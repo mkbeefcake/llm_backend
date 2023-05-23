1d3abe779eb31c52bd05bab94ffe224b6d2a5c8a
mkbeefcake <mkblockchaindev@outlook.com>
2023-05-24 00:45:50 +0800

    20230524: added signup mechanism

--- apis/users.py
+++ apis/users.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
-from core.firebase import authenticate_user, decode_access_token
+from core.firebase import authenticate_user, decode_access_token, create_user
+from helpers.forms import BasicAuthenticationForm
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
@@ -20,10 +21,13 @@ async def me(current_user: User = Depends(get_current_user)):
 
 @router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):  
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
+    access_token = authenticate_user(form_data.username, form_data.password)
+    if not access_token:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
 
-    access_token = user['idToken']
     return {"access_token": access_token, "token_type": "bearer"}
 
+@router.post("/signup")
+async def signup(form_data: BasicAuthenticationForm = Depends()):
+    user = create_user(form_data.username, form_data.password)
+    return {"message": "User created successfully"}
--- core/firebase.py
+++ core/firebase.py
@@ -23,7 +23,7 @@ db = firestore.client()
 def authenticate_user(email: str, password: str):
     try:
         auth_user = pb.auth().sign_in_with_email_and_password(email, password)
-        return auth_user
+        return auth_user['idToken']
     except:
         return None
     
@@ -31,5 +31,13 @@ def decode_access_token(token: str):
     try:
         user = auth.verify_id_token(token)
         return user
+    except:
+        return None
+    
+def create_user(email: str, password: str):
+    try:
+        user = pb.auth().create_user_with_email_and_password(email=email, password=password)
+        result = pb.auth().send_email_verification(user['idToken'])
+        return result
     except:
         return None
