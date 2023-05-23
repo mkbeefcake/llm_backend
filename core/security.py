8d10f41ff635b1e97d1d7f77a89994c9bd6e1a0a
mkbeefcake <mkblockchaindev@outlook.com>
2023-05-23 18:02:09 +0800

    20230523: added debug environment, load .env variable for setting

--- core/security.py
+++ core/security.py
@@ -19,13 +19,13 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, "settings.SECRET_KEY", algorithm=settings.ALGORITHM
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
     return encoded_jwt
 
 def decode_access_token(token:str):
     try:
-        payload = jwt.decode(token, "settings.SECRET_KEY", algorithm=settings.ALGORITHM)
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
     
