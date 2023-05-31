2f868e68288b0dffe451e57f263a9aed0ad5fc25
mkbeefcake <mkblockchaindev@outlook.com>
2023-05-31 09:07:04 +0800

    20230531: probe function for startup TCP probe on GCR

--- main.py
+++ main.py
@@ -33,5 +33,10 @@ def application_start():
 app = application_start()
 
 
+@app.get("/probe")
+def probe():
+    return {"message": "ok"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
