61cb36ffa5c602cd6cf207dc9c2dc190734e01d6
mkbeefcake <mkblockchaindev@outlook.com>
2023-06-15 01:08:04 +0800

    20230615: migrated pinecone init from __init__ to other function

--- products/pinecone.py
+++ products/pinecone.py
@@ -19,23 +19,36 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 class PineconeService(ProductService):
     def __init__(self) -> None:
-        # Pinecone initialize index
-        pinecone.init(api_key=PINECONE_API_KEY, environment="us-east1-gcp")
+        self.initialized = False
+        pass
 
-        dimension = 1536
-        index_name = "import-product"
-        if index_name not in pinecone.list_indexes():
-            print("Index is not existed")
+    def initialize(self):
+        if self.initialized == True:
             return
 
-        # Get the vectorstore and add new products
-        self.index = pinecone.Index(index_name)
-        self.openai = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-        self.vectorstore = Pinecone(self.index, self.openai.embed_query, "text")
+        try: 
+            # Pinecone initialize index
+            pinecone.init(api_key=PINECONE_API_KEY, environment="us-east1-gcp")
 
+            dimension = 1536
+            index_name = "import-product"
+            if index_name not in pinecone.list_indexes():
+                print("Index is not existed")
+                return
+
+            # Get the vectorstore and add new products
+            self.index = pinecone.Index(index_name)
+            self.openai = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+            self.vectorstore = Pinecone(self.index, self.openai.embed_query, "text")
+            
+        except Exception as e:
+            print (f"Exception in PineconeService: {str(e)}")
+
+        self.initialized = True
         pass
 
     def match_product(self, messages: str):
+        self.initialize()
         if self.vectorstore is None:
             return "Error happened to connect pinecone"
 
