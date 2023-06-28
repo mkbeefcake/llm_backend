6ed6bb54f33f157464de7a159e11d531f4f44791
mkbeefcake <mkblockchaindev@outlook.com>
2023-06-29 02:00:55 +0800

    20230629: fixed #2

--- db/cruds/purchased.py
+++ db/cruds/purchased.py
@@ -17,71 +17,60 @@ def update_purchased(user_id: str, provider_name: str, key: str, content: any):
     if not purchased_doc.exists:
         purchased_doc_ref.set({})
 
-    purchased_data = purchased_doc_ref.get().val()
+    purchased_data = purchased_doc_ref.get().to_dict()
 
-    # if content == "":
-    #     pass
-    #     # del purchased_data[provider_name][key]
-    # else:
-    #     updated = []
+    if content == "":
+        pass
+        # del purchased_data[provider_name][key]
+    else:
+        for chatuser_id in purchased_data:
+            if chatuser_id in content and "statistics" in content[chatuser_id]:
+                purchased_data[chatuser_id]["statistics"] = content[chatuser_id][
+                    "statistics"
+                ]
 
-    #     # iterate original content
-    #     try:
-    #         for original in purchased_data[provider_name][key]:
-    #             # skip statistics item
-    #             if "message_id" not in original:
-    #                 continue
+            if chatuser_id in content and "purchased" in content[chatuser_id]:
+                original_content = purchased_data[chatuser_id]["purchased"]
+                newest_content = content[chatuser_id]["purchased"]
 
-    #             org_message_id = original["message_id"]
-    #             found = False
+                updated = []
 
-    #             for newest in content:
-    #                 # skip statistics item
-    #                 if "message_id" not in newest:
-    #                     continue
+                # iterate original content
+                try:
+                    for original in original_content:
+                        org_message_id = original["message_id"]
+                        found = False
 
-    #                 new_message_id = newest["message_id"]
-    #                 if org_message_id == new_message_id:
-    #                     updated.append(newest)
-    #                     found = True
-    #                     break
+                        for newest in newest_content:
+                            new_message_id = newest["message_id"]
+                            if org_message_id == new_message_id:
+                                updated.append(newest)
+                                found = True
+                                break
 
-    #             if found == False:
-    #                 updated.append(original)
-    #     except Exception as e:
-    #         pass
+                        if found == False:
+                            updated.append(original)
+                except Exception as e:
+                    pass
 
-    #     # iterate new content
-    #     try:
-    #         for newest in content:
-    #             # add new statistics item
-    #             if "message_id" not in newest:
-    #                 updated.append(newest)
-    #                 continue
+                # iterate new content
+                try:
+                    for newest in newest_content:
+                        new_message_id = newest["message_id"]
+                        found = False
 
-    #             new_message_id = newest["message_id"]
-    #             found = False
+                        for current in updated:
+                            current_message_id = current["message_id"]
+                            if new_message_id == current_message_id:
+                                found = True
+                                break
 
-    #             for current in updated:
-    #                 # skip statistics item
-    #                 if "message_id" not in current:
-    #                     continue
+                        if found == False:
+                            updated.append(newest)
+                except Exception as e:
+                    pass
 
-    #                 current_message_id = current["message_id"]
-    #                 if new_message_id == current_message_id:
-    #                     found = True
-    #                     break
+                purchased_data[chatuser_id]["purchased"] = updated
 
-    #             if found == False:
-    #                 updated.append(newest)
-    #     except Exception as e:
-    #         pass
-
-    #     purchased_data[provider_name][key] = updated
-
-    # purchased_doc_ref.update(
-    #     {
-    #         provider_name: purchased_data[provider_name],
-    #     }
-    # )
+        purchased_doc_ref.update(purchased_data)
     return {"message": "Purchased data updated successfully"}
