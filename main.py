41889f0b66f0316cea0fe167559bdae3c73d3b80
Matthieu <matthieu@matthieus-mbp.home>
2023-07-29 16:32:04 +0100

    Revert "20230728: integrate prometheus for metric"
    This reverts commit 2b4b921c0a987e549e18cd8bc9993653fc6c1501.
    Error with prometheus in the backend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from prometheus_fastapi_instrumentator import Instrumentator
 from pydantic import BaseModel
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -51,7 +50,6 @@ def application_start():
 
 
 app = application_start()
-Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/probe")
