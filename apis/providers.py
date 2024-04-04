5821dee31df0ded29b388fd3703d68cc70158501
mkbeefcake <mkblockchaindev@outlook.com>
2024-04-03 22:10:21 -0400

    removed pipeline for test

--- apis/providers.py
+++ apis/providers.py
@@ -87,20 +87,24 @@ async def unlink_Provider(
 
         user_id = curr_user["uid"]
         
-        response = requests.post(
-            f"https://chat-automation-387710-purchased-yix5m2x4pq-uc.a.run.app/pipeline/stop_purchased_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        #
+        # Deprecated the pipeline
+        #
 
-        response = requests.post(
-            f"https://chat-automation-387710-products-yix5m2x4pq-uc.a.run.app/pipeline/stop_all_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        # response = requests.post(
+        #     f"https://chat-automation-387710-purchased-yix5m2x4pq-uc.a.run.app/pipeline/stop_purchased_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
 
-        response = requests.post(
-            f"https://chat-automation-387710-scrapy-yix5m2x4pq-uc.a.run.app/pipeline/stop_chat_history_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        # response = requests.post(
+        #     f"https://chat-automation-387710-products-yix5m2x4pq-uc.a.run.app/pipeline/stop_all_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
+
+        # response = requests.post(
+        #     f"https://chat-automation-387710-scrapy-yix5m2x4pq-uc.a.run.app/pipeline/stop_chat_history_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
 
         return MessageOK(
             data=update_user(
@@ -144,20 +148,24 @@ async def update_provider_info(
             user_data[provider_name][identifier_name],
         )
 
-        response = requests.post(
-            f"https://chat-automation-387710-purchased-yix5m2x4pq-uc.a.run.app/pipeline/fetch_purchased_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        #
+        # Deprecated the pipeline
+        #
 
-        response = requests.post(
-            f"https://chat-automation-387710-products-yix5m2x4pq-uc.a.run.app/pipeline/fetch_all_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        # response = requests.post(
+        #     f"https://chat-automation-387710-purchased-yix5m2x4pq-uc.a.run.app/pipeline/fetch_purchased_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
 
-        response = requests.post(
-            f"https://chat-automation-387710-scrapy-yix5m2x4pq-uc.a.run.app/pipeline/get_chat_history_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
-        )
-        response_json = response.json()
+        # response = requests.post(
+        #     f"https://chat-automation-387710-products-yix5m2x4pq-uc.a.run.app/pipeline/fetch_all_products_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
+
+        # response = requests.post(
+        #     f"https://chat-automation-387710-scrapy-yix5m2x4pq-uc.a.run.app/pipeline/get_chat_history_for_one?user_id={user_id}&provider_name={provider_name}&identifier_name={identifier_name}"
+        # )
+        # response_json = response.json()
 
         return MessageOK(data=result)
     except Exception as e:
